198598e249540185cc8dcffa5e065279c4944488
Alexandre <0x41e@protonmail.ch>
2016-11-17 16:27:13 -0200

    Add test_decorator.py

---
+++ tests/unit/test_decorator.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+test_decorator
+----------------------------------
+
+Tests for `sqlalchemy_test_cache.decorator` module.
+"""
+
+import unittest
+
+
+class DecoratorTestCase(unittest.TestCase):
+    pass
